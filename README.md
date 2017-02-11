916c87edd26fa8d7b0d9d14777bbf852cc2633fe
Sergey V. Zhdanovskih <serg.zhdanovskih@yandex.ru>
2017-02-11 18:43:47 +0500

    Batch for cleaning, update readme

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-nVLC
-====
+# nVLC
 
-A wrapper library in C# for interacting with VLC
+A wrapper library in C# for .NET (WPF & WinForms) and Mono (WinForms),
+for interacting with VLC, based on [nVLC](http://www.codeproject.com/Articles/109639/nVLC).
---
+++ clean.cmd
@@ -0,0 +1,12 @@
+
+rmdir .\Sources\nVLC\bin /s /q
+rmdir .\Sources\nVLC\obj /s /q
+
+rmdir .\Sources\nVLC_Demo_WinForms\bin /s /q
+rmdir .\Sources\nVLC_Demo_WinForms\obj /s /q
+
+rmdir .\Sources\nVLC_Demo_WPF\bin /s /q
+rmdir .\Sources\nVLC_Demo_WPF\obj /s /q
+
+rmdir .\Sources\nVLC_Demo_WPF_D3DImage\bin /s /q
+rmdir .\Sources\nVLC_Demo_WPF_D3DImage\obj /s /q
