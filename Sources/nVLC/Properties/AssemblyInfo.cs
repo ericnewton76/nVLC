7586f01922c9ddcdc39c2e2440ea2129e93b560c
Sergey V. Zhdanovskih <serg.zhdanovskih@yandex.ru>
2017-02-11 15:48:03 +0500

    Cleaning references and updating project files

--- Sources/nVLC/Properties/AssemblyInfo.cs
+++ Sources/nVLC/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Implementation")]
+[assembly: AssemblyTitle("nVLC")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Implementation")]
+[assembly: AssemblyProduct("nVLC")]
 [assembly: AssemblyCopyright("Copyright © Roman Ginzburg 2010")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
